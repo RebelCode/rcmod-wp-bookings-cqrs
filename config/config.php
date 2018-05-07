67f7296a73d28213b8bd6203c9c025f6d892cb69
Miguel Muscat <miguelmuscat93@gmail.com>
2018-05-07 13:33:04 +0200

    Added session rules config

--- config/config.php
+++ config/config.php
@@ -11,6 +11,7 @@ return [
         'table_prefix'    => '${wpdb_prefix}',
         'bookings'        => include __DIR__ . '/bookings.php',
         'sessions'        => include __DIR__ . '/sessions.php',
+        'session_rules'   => include __DIR__ . '/session_rules.php',
         'transition_logs' => include __DIR__ . '/transition_log.php',
     ],
 ];
---
+++ config/session_rules.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+    // Config for all session rule RMs
+    'table'            => '${cqrs/table_prefix}session_rules',
+    'field_column_map' => $sessionRulesFieldColumnMap = [
+        'id'                  => 'id',
+        'service_id'          => 'service_id',
+        'start'               => 'start',
+        'end'                 => 'end',
+        'all_day'             => 'all_day',
+        'repeat'              => 'repeat',
+        'repeat_period'       => 'repeat_period',
+        'repeat_unit'         => 'repeat_unit',
+        'repeat_until'        => 'repeat_until',
+        'repeat_until_date'   => 'repeat_until_date',
+        'repeat_until_period' => 'repeat_until_period',
+        'repeat_daily_on'     => 'repeat_daily_on',
+        'repeat_monthly_on'   => 'repeat_monthly_on',
+        'exclude_dates'       => 'exclude_dates',
+    ],
+
+    // Config for SELECT RMs
+    'select'           => [
+        'tables'           => ['session_rule' => '${cqrs/session_rules/table}'],
+        'field_column_map' => $sessionRulesFieldColumnMap,
+        'joins'            => [],
+    ],
+
+    // Config for INSERT RMs
+    'insert'           => [
+        'table'            => '${cqrs/session_rules/table}',
+        'field_column_map' => $sessionRulesFieldColumnMap,
+        'insert_bulk'      => true,
+    ],
+
+    // Config for UPDATE RMs
+    'update'           => [
+        'table'            => '${cqrs/session_rules/table}',
+        'field_column_map' => $sessionRulesFieldColumnMap,
+    ],
+
+    // Config for DELETE RMs
+    'delete'           => [
+        'table'            => '${cqrs/session_rules/table}',
+        'field_column_map' => $sessionRulesFieldColumnMap,
+    ],
+];
