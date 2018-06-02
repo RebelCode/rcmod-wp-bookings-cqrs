6413187a729fdca833e58f96484d7900ad76c98c
Miguel Muscat <miguelmuscat93@gmail.com>
2018-06-02 17:32:32 +0200

    Added initial migration SQL files

---
+++ migrations/1-down.sql
@@ -0,0 +1,7 @@
+drop table if exists ${table_prefix}booking_status_logs;
+
+drop table if exists ${table_prefix}bookings;
+
+drop table if exists ${table_prefix}session_rules;
+
+drop table if exists ${table_prefix}sessions;
