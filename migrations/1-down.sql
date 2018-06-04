f1d065d74f43a2128ac37d0c8a6aa20be6ec265c
Miguel Muscat <miguelmuscat93@gmail.com>
2018-06-04 18:09:13 +0200

    Quoted placeholders in migration SQL files

--- migrations/1-down.sql
+++ migrations/1-down.sql
@@ -1,5 +1,5 @@
-drop table if exists ${cqrs/bookings/table};
+drop table if exists `${cqrs/bookings/table}`;
 
-drop table if exists ${cqrs/session_rules/table};
+drop table if exists `${cqrs/session_rules/table}`;
 
-drop table if exists ${cqrs/sessions/table};
+drop table if exists `${cqrs/sessions/table}`;
