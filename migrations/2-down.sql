639381ddcdddacf0793f0620ef9b960a5a758da8
Miguel Muscat <miguelmuscat93@gmail.com>
2018-08-17 12:38:05 +0200

    Properly escaping migrations

--- migrations/2-down.sql
+++ migrations/2-down.sql
@@ -1,3 +1,3 @@
-alter table `${cqrs/session_rules/table}` modify repeat_monthly_on enum('dotw', 'dotm');
-alter table `${cqrs/session_rules/table}` modify repeat_unit enum('days', 'weeks', 'months', 'years');
-alter table `${cqrs/session_rules/table}` modify repeat_until enum('date', 'period');
+alter table `${cqrs/session_rules/table}` modify `repeat_monthly_on` enum('dotw', 'dotm');
+alter table `${cqrs/session_rules/table}` modify `repeat_unit` enum('days', 'weeks', 'months', 'years');
+alter table `${cqrs/session_rules/table}` modify `repeat_until` enum('date', 'period');
