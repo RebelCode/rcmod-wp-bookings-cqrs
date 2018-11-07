76389df51d4571fee72f33959b4a6b48013cd9f3
Miguel Muscat <miguelmuscat93@gmail.com>
2018-11-07 09:10:43 +0100

    Added SQL migration comments

--- migrations/3-up.sql
+++ migrations/3-up.sql
@@ -1,7 +1,9 @@
+-- Rename session lengths to session types
 UPDATE `${wpdb_prefix}postmeta`
 SET `meta_key` = "eddbk_session_types"
 WHERE `meta_key` = "eddbk_session_lengths";
 
+-- Create resources table
 CREATE TABLE `${cqrs/resources/table}`
 (
 	id bigint AUTO_INCREMENT PRIMARY KEY,
