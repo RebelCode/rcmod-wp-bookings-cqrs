5fd44f115a4527140a8920ea203d80e7af1f0877
Miguel Muscat <miguelmuscat93@gmail.com>
2018-11-07 09:10:09 +0100

    Adds migration that creates the resources table
    EDDBK-172

--- migrations/3-up.sql
+++ migrations/3-up.sql
@@ -1,3 +1,11 @@
 UPDATE `${wpdb_prefix}postmeta`
 SET `meta_key` = "eddbk_session_types"
 WHERE `meta_key` = "eddbk_session_lengths";
+
+CREATE TABLE `${cqrs/resources/table}`
+(
+	id bigint AUTO_INCREMENT PRIMARY KEY,
+	type varchar(100) NOT NULL,
+	name varchar(255) NOT NULL
+);
+
