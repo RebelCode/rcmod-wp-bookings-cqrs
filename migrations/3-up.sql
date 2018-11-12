116439e2dde76ef3cd743fd60eb4239b9fcf57cf
Miguel Muscat <miguelmuscat93@gmail.com>
2018-11-12 12:19:39 +0100

    Escaped all column names in v3 up migration
    EDDBK-148

--- migrations/3-up.sql
+++ migrations/3-up.sql
@@ -6,25 +6,25 @@ WHERE `meta_key` = "eddbk_session_lengths";
 -- Create resources table
 CREATE TABLE `${cqrs/resources/table}`
 (
-	id bigint AUTO_INCREMENT PRIMARY KEY,
-	type varchar(100) NOT NULL,
-	name varchar(255) NOT NULL
+	`id` bigint AUTO_INCREMENT PRIMARY KEY,
+	`type` varchar(100) NOT NULL,
+	`name` varchar(255) NOT NULL
 );
 
 -- Create session-resources relationship table
 CREATE TABLE `${cqrs/booking_resources/table}`
 (
-  id int AUTO_INCREMENT PRIMARY KEY,
-  booking_id int NOT NULL,
-  resource_id int NOT NULL
+  `id` bigint AUTO_INCREMENT PRIMARY KEY,
+  `booking_id` int NOT NULL,
+  `resource_id` int NOT NULL
 );
 
 -- Create session-resources relationship table
 CREATE TABLE `${cqrs/session_resources/table}`
 (
-  id int AUTO_INCREMENT PRIMARY KEY,
-  session_id int NOT NULL,
-  resource_id int NOT NULL
+  `id` bigint AUTO_INCREMENT PRIMARY KEY,
+  `session_id` int NOT NULL,
+  `resource_id` int NOT NULL
 );
 
 -- Create schedule resources for every service
@@ -34,15 +34,15 @@ FROM `${wpdb_prefix}posts`
 WHERE `post_type` = "download";
 
 -- Populate booking resources table with existing booking.resource_id data
-INSERT INTO `${cqrs/booking_resources/table}` (booking_id, resource_id)
-SELECT id as booking_id, resource_id
+INSERT INTO `${cqrs/booking_resources/table}` (`booking_id`, `resource_id`)
+SELECT `id` as booking_id, `resource_id`
 FROM `${cqrs/bookings/table}`;
 -- Remove `resource_id` column from bookings table
 ALTER TABLE `${cqrs/bookings/table}` DROP COLUMN `resource_id`;
 
 -- Populate session resources table with existing session.resource_id data
-INSERT INTO `${cqrs/session_resources/table}` (session_id, resource_id)
-SELECT id as session_id, resource_id
+INSERT INTO `${cqrs/session_resources/table}` (`session_id`, `resource_id`)
+SELECT `id` as session_id, `resource_id`
 FROM `${cqrs/sessions/table}`;
 -- Remove `resource_id` column from sessions table
 ALTER TABLE `${cqrs/sessions/table}` DROP COLUMN `resource_id`;
