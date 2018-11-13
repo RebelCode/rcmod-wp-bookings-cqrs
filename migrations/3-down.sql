ca31dce3b2243646ae72f397f5cf69b969d6d99f
Miguel Muscat <miguelmuscat93@gmail.com>
2018-11-13 16:50:02 +0100

    DDL queries in migrations are executed first
    EDDBK-148

--- migrations/3-down.sql
+++ migrations/3-down.sql
@@ -1,12 +1,12 @@
+-- Delete the resources table
+DROP TABLE IF EXISTS `${cqrs/resources/table}`;
+-- Delete the bookings-resources relationship table
+DROP TABLE IF EXISTS `${cqrs/booking_resources/table}`;
+
 UPDATE `${wpdb_prefix}postmeta`
 SET `meta_key` = "eddbk_session_lengths"
 WHERE `meta_key` = "eddbk_session_types";
 
--- Delete the resources table
-DROP TABLE IF EXISTS `${cqrs/resources/table}`;
-
--- Delete the bookings-resources relationship table
-DROP TABLE IF EXISTS `${cqrs/booking_resources/table}`;
 -- Re-add the resource_id column to the bookings table
 ALTER table `${cqrs/bookings/table}` ADD `resource_id` int NOT NULL;
 -- Set all resource IDs in the bookings table to be equal to service IDs
--- migrations/3-up.sql
+++ migrations/3-up.sql
@@ -1,8 +1,3 @@
--- Rename session lengths to session types
-UPDATE `${wpdb_prefix}postmeta`
-SET `meta_key` = "eddbk_session_types"
-WHERE `meta_key` = "eddbk_session_lengths";
-
 -- Create resources table
 CREATE TABLE `${cqrs/resources/table}`
 (
@@ -10,14 +5,7 @@ CREATE TABLE `${cqrs/resources/table}`
 	`type` varchar(100) NOT NULL,
 	`name` varchar(255) NOT NULL
 );
-
--- Create schedule resources for every service
-INSERT INTO `${cqrs/resources/table}` (`id`, `type`, `name`)
-SELECT `ID`, "schedule", CONCAT("Schedule for \"", post_title, "\"")
-FROM `${wpdb_prefix}posts`
-WHERE `post_type` = "download";
-
--- Create session-resources relationship table
+-- Create booking-resources relationship table
 CREATE TABLE `${cqrs/booking_resources/table}`
 (
   `id` bigint AUTO_INCREMENT PRIMARY KEY,
@@ -25,9 +13,20 @@ CREATE TABLE `${cqrs/booking_resources/table}`
   `resource_id` int NOT NULL
 );
 
+-- Rename session lengths to session types
+UPDATE `${wpdb_prefix}postmeta`
+SET `meta_key` = "eddbk_session_types"
+WHERE `meta_key` = "eddbk_session_lengths";
+
+-- Create schedule resources for every service
+INSERT INTO `${cqrs/resources/table}` (`id`, `type`, `name`)
+SELECT `ID`, "schedule", CONCAT("Schedule for \"", post_title, "\"")
+FROM `${wpdb_prefix}posts`
+WHERE `post_type` = "download";
+
 -- Populate booking resources table with existing booking.resource_id data
 INSERT INTO `${cqrs/booking_resources/table}` (`booking_id`, `resource_id`)
-SELECT `id` as booking_id, `resource_id`
+SELECT `id` as `booking_id`, `resource_id`
 FROM `${cqrs/bookings/table}`;
 -- Remove `resource_id` column from bookings table
 ALTER TABLE `${cqrs/bookings/table}` DROP COLUMN `resource_id`;
