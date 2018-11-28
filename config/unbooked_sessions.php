6e0d254ad19a06dc0d4360df0b89a6bf274beaae
Miguel Muscat <miguelmuscat93@gmail.com>
2018-11-28 19:11:16 +0100

    Added temporary fix for "unknown column" errors
    EDDBK-148

--- config/unbooked_sessions.php
+++ config/unbooked_sessions.php
@@ -8,7 +8,10 @@ return [
         'start'        => ['session', 'start'],
         'end'          => ['session', 'end'],
         'service_id'   => ['session', 'service_id'],
-        'resource_ids' => ['session', 'resource_ids']
+        'resource_ids' => ['session', 'resource_ids'],
+        // Temporary solution for "unknown column" errors in SQL queries
+        'booking_id'     => ['${cqrs/bookings/table}', 'id'],
+        'booking_status' => ['${cqrs/bookings/table}', 'status'],
     ],
 
     // Config for SELECT RMs
